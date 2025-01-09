169d00c88ba98b8c8a3944bb8d748aade81dbd7d
gabriel 2 <jfernandezsantos@bstonk12.org>
2025-01-09 09:30:10 -0500

    1-8-2025

---
+++ 2024-2025/Week_9/2025-01-07/sum.py
@@ -0,0 +1,10 @@
+# gabriel
+#2025-8-1
+
+num1=int(input('Enter a number: '))
+num2=int(input('Enter another number: '))
+num3=int(input('Enter a third number: '))
+
+product = num1*num2*num3
+print(product)
+print(num1,"X",num2,"X",num3,"=",product)
