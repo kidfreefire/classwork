7de2203a0d0bb7d93c2e8fc5d807245323a2a9c7
gabriel 2 <jfernandezsantos@bstonk12.org>
2025-01-09 10:33:58 -0500

    tarea

---
+++ 2024-2025/2025-01-09/called if.py
@@ -0,0 +1,9 @@
+#gabriel
+#2025-9-1
+
+num1=int(input('Ente a number: '))
+num2=int(input('Ente a number: '))
+if num1>num2:
+    print("biggest number that has been seen so far is ",num1)
+else:
+    print("smaller number that has been seen so far is ",num2)
